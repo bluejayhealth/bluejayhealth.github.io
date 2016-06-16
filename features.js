8ba35688391dd8f637c55e3a3173395bd2ac875a
bluejayhealth <interm@bj1s-Mac-mini.local>
2016-06-16 13:59:16 -0700

    mobile try 3
    adf

--- features.js
+++ features.js
@@ -1,89 +1,51 @@
-
-if (window.width < 768 px) {
-    $(function() {
-        $(window).scroll( function(){
-        
-           
-            $('.fadeInBlock2').each( function(i){
-                
-                var bottom_of_object = $(this).position().top + $(this).outerHeight();
-                var bottom_of_window = $(window).scrollTop() + $(window).height();
-                
-                bottom_of_window = bottom_of_window - 2300;  
-              
-                if( bottom_of_window > bottom_of_object ){
-                    
-                    $(this).animate({'opacity':'1'},1200);
-                        
-                }
-            }); 
-        
-        });
-    });
+if ($(window).width() < 768) {
+   var feature1 = 800;
+   var feature2 = 1600
 }
 else {
-    $(function() {
-        $(window).scroll( function(){
-        
-           
-            $('.fadeInBlock2').each( function(i){
-                
-                var bottom_of_object = $(this).position().top + $(this).outerHeight();
-                var bottom_of_window = $(window).scrollTop() + $(window).height();
-                
-                bottom_of_window = bottom_of_window - 2300;  
-              
-                if( bottom_of_window > bottom_of_object ){
-                    
-                    $(this).animate({'opacity':'1'},1200);
-                        
-                }
-            }); 
-        
-        });
-    });
+   var feature1 = 1300
+   var feature2 = 2300
 }
-if (window.width < 768px) {
-    $(function() {
-        $(window).scroll( function(){
 
-           
-            $('.fadeInBlock').each( function(i){
-                
-                var bottom_of_object = $(this).position().top + $(this).outerHeight();
-                var bottom_of_window = $(window).scrollTop() + $(window).height();
-                
-                bottom_of_window = bottom_of_window - 1300;  
-              
-                if( bottom_of_window > bottom_of_object ){
-                    
-                    $(this).animate({'opacity':'1'},1200);
-                        
-                }
-            }); 
 
-        });
-    });
-}
-else {
-    $(function() {
-        $(window).scroll( function(){
-        
-           
-            $('.fadeInBlock').each( function(i){
+$(function() {
+    $(window).scroll( function(){
+    
+       
+        $('.fadeInBlock').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            bottom_of_window = bottom_of_window - feature1;  
+          
+            if( bottom_of_window > bottom_of_object ){
                 
-                var bottom_of_object = $(this).position().top + $(this).outerHeight();
-                var bottom_of_window = $(window).scrollTop() + $(window).height();
-                
-                bottom_of_window = bottom_of_window - 1300;  
-              
-                if( bottom_of_window > bottom_of_object ){
+                $(this).animate({'opacity':'1'},1200);
                     
-                    $(this).animate({'opacity':'1'},1200);
-                        
-                }
-            }); 
-        
-        });
+            }
+        }); 
+    
     });
-}
+});
+
+$(function() {
+    $(window).scroll( function(){
+    
+       
+        $('.fadeInBlock2').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            bottom_of_window = bottom_of_window - feature2;  
+          
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},1200);
+                    
+            }
+        }); 
+    
+    });
+});
